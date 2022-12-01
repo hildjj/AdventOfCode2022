66a606a59ffb90466815edeff6e79996dfd38785
Joe Hildebrand <joe-github@cursive.net>
2022-12-01 14:15:50 -0700

    Clean up day 1

--- day1.ts
+++ day1.ts
@@ -1,18 +1,20 @@
 #!/usr/bin/env node --loader ts-node/esm
+import { NumberSequence } from "./sequence.js";
 import Utils from "./utils.js"; // Really .ts
 
 function part1(elves: number[]): number {
-  return Math.max(...elves);
+  return elves[0];
 }
 
 function part2(elves: number[]): number {
-  elves.sort((a, b) => b - a);
-  return elves[0] + elves[1] + elves[2];
+  return new NumberSequence(elves).take(3).sum();
 }
 
 export default function main(inFile: string, trace: boolean) {
   const inp: number[][] = Utils.parseFile(inFile, undefined, trace);
   const elves = inp.map(elf => elf.reduce((t, v) => t + v, 0));
+  // Descending order
+  elves.sort((a, b) => b - a);
   return [part1(elves), part2(elves)];
 }
 
