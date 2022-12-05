83b2bc67518ebf977cf88b7f4df068ceabe9f8ad
Joe Hildebrand <joe-github@cursive.net>
2022-12-04 22:47:56 -0700

    day 5

---
+++ day5.ts
@@ -0,0 +1,38 @@
+#!/usr/bin/env node --loader ts-node/esm
+import Utils from "./utils.js"; // Really .ts
+
+// Parser outputs this:
+interface Plan {
+  stacks: string[][];
+  moves: number[][];
+}
+
+function deepCopy<T>(nest: T): T {
+  return JSON.parse(JSON.stringify(nest)) as T;
+}
+
+function part1(inp: Plan): string {
+  const stacks = deepCopy(inp.stacks);
+  for (const [size, from, to] of inp.moves) {
+    const crates = stacks[from - 1].splice(0, size);
+    crates.reverse();
+    stacks[to - 1].splice(0, 0, ...crates);
+  }
+  return stacks.map(s => s[0]).join("");
+}
+
+function part2(inp: Plan): string {
+  const stacks = deepCopy(inp.stacks);
+  for (const [size, from, to] of inp.moves) {
+    const crates = stacks[from - 1].splice(0, size);
+    stacks[to - 1].splice(0, 0, ...crates);
+  }
+  return stacks.map(s => s[0]).join("");
+}
+
+export default function main(inFile: string, trace: boolean) {
+  const inp: Plan = Utils.parseFile(inFile, undefined, trace);
+  return [part1(inp), part2(inp)];
+}
+
+Utils.main(import.meta.url, main);
